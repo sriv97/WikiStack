1b9a0fd1e4ed42bed1ae21c55b41761a84449d37
Sri Velagapudi <srivar1997@gmail.com>
2019-04-25 11:56:11 -0400

    added view modules

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const morgan = require("morgan");
 const express = require("express");
 const bodyParser = require("body-parser");
+const layout = require("./views/layout");
 
 const app = express();
 
@@ -11,10 +12,11 @@ app.use(morgan("dev"));
 
 
 app.get("/", (req,res,next) => {
+	res.send(layout('Hi'));
 	console.log("Hello World!");
 });
 
-const port = 1337;
+const port = 3000;
 
 app.listen(port, () => {
 	console.log(`wikistack is listning on port ${port}`);
